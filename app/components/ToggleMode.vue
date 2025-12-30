4f45a85c66e01c39618d9769d530eebf75cdff79
bayuwijanarko07 <bayuwijanarko07@gmail.com>
2025-12-30 17:27:32 +0700

    update layouts components

---
+++ app/components/ToggleMode.vue
@@ -0,0 +1,12 @@
+<script setup>
+const colorMode = useColorMode()
+</script>
+
+<template>
+  <button
+    @click="colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'"
+  >
+    <span v-if="colorMode.value === 'dark'">🌙</span>
+    <span v-else>☀️</span>
+  </button>
+</template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bg-gray-50 text-gray-800 dark:bg-gray-950 dark:text-gray-100 min-h-screen">
+
+    <PageContainer class="py-[68px] border-b dark:border-gray-100 border-gray-950">
+      <header>
+        Header
+       <ToggleMode/>
+      </header>
+      <main>
+        <slot />
+      </main>
+      <footer>
+        Footer
+      </footer>
+    </PageContainer>
+  </div>
+</template>
