bbbf5dd92af3377d2266a026f5b96be0c716f9ac
bayuwijanarko07 <bayuwijanarko07@gmail.com>
2026-01-27 16:57:23 +0700

    fix debug github

--- server/api/github.post.ts
+++ server/api/github.post.ts
@@ -1,36 +1,26 @@
-// import { GITHUB_ACCOUNT, GITHUB_USER_QUERY } from '@/common/constants/github'
-// import { githubAuthHeader } from '../utils/githubAuthHeader'
-// import type { GithubGraphQLResponse } from '@/types/github'
+import { GITHUB_ACCOUNT, GITHUB_USER_QUERY } from '@/common/constants/github'
+import { githubAuthHeader } from '../utils/githubAuthHeader'
+import type { GithubGraphQLResponse } from '@/types/github'
 
-// export default cachedEventHandler(async () => {
-//     const username = GITHUB_ACCOUNT.username
+export default cachedEventHandler(async () => {
+    const username = GITHUB_ACCOUNT.username
 
-//     const res = await $fetch<GithubGraphQLResponse>(
-//       GITHUB_ACCOUNT.user_endpoint,
-//       {
-//         method: 'POST',
-//         headers: githubAuthHeader(),
-//         body: {
-//           query: GITHUB_USER_QUERY,
-//           variables: { username },
-//         },
-//       }
-//     )
-//     return res.data.user.contributionsCollection.contributionCalendar
-//   },
-//   // {
-//   //   maxAge: 60 * 60, 
-//   //   staleMaxAge: 60 * 60 * 24,
-//   //   name: 'github-contribution-calendar',
-//   // }
-// )
-
-
-export default defineEventHandler(async () => {
-  const config = useRuntimeConfig()
-
-  console.log('TOKEN EXISTS:', !!config.githubToken)
-  console.log('TOKEN LENGTH:', config.githubToken?.length)
-
-  return config.githubToken
-})
+    const res = await $fetch<GithubGraphQLResponse>(
+      GITHUB_ACCOUNT.user_endpoint,
+      {
+        method: 'POST',
+        headers: githubAuthHeader(),
+        body: {
+          query: GITHUB_USER_QUERY,
+          variables: { username },
+        },
+      }
+    )
+    return res.data.user.contributionsCollection.contributionCalendar
+  },
+  // {
+  //   maxAge: 60 * 60, 
+  //   staleMaxAge: 60 * 60 * 24,
+  //   name: 'github-contribution-calendar',
+  // }
+)
