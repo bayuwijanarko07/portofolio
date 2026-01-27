33b60981063a634126af276d3b782f0eaa6db98e
bayuwijanarko07 <bayuwijanarko07@gmail.com>
2026-01-27 15:11:31 +0700

    update fetch

--- server/api/github.post.ts
+++ server/api/github.post.ts
@@ -1,25 +1,42 @@
 import { GITHUB_ACCOUNT, GITHUB_USER_QUERY } from '@/common/constants/github'
-import { githubAuthHeader } from '../utils/githubAuthHeader'
 import type { GithubGraphQLResponse } from '@/types/github'
 
-export default cachedEventHandler(async () => {
-    const username = GITHUB_ACCOUNT.username
+export default cachedEventHandler(
+  async () => {
+    try {
+      const { githubToken } = useRuntimeConfig()
 
-    const res = await $fetch<GithubGraphQLResponse>(
-      GITHUB_ACCOUNT.user_endpoint,
-      {
-        method: 'POST',
-        headers: githubAuthHeader(),
-        body: {
-          query: GITHUB_USER_QUERY,
-          variables: { username },
-        },
+      if (!githubToken) {
+        throw new Error('GitHub token missing')
       }
-    )
-    return res.data.user.contributionsCollection.contributionCalendar
+
+      const res = await $fetch<GithubGraphQLResponse>(
+        GITHUB_ACCOUNT.user_endpoint,
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${githubToken}`,
+            'User-Agent': 'Nuxt-Cloudflare-App',
+            'Content-Type': 'application/json',
+          },
+          body: {
+            query: GITHUB_USER_QUERY,
+            variables: { username: GITHUB_ACCOUNT.username },
+          },
+        }
+      )
+
+      return res.data.user.contributionsCollection.contributionCalendar
+    } catch (e) {
+      console.error('GitHub API error:', e)
+      throw createError({
+        statusCode: 500,
+        statusMessage: 'GitHub GraphQL failed',
+      })
+    }
   },
   {
-    maxAge: 60 * 60, 
+    maxAge: 60 * 60,
     staleMaxAge: 60 * 60 * 24,
     name: 'github-contribution-calendar',
   }
